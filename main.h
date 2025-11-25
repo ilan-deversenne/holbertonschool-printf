50679e0260deed3847d45510a3181bde697f74c9
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-25 10:18:12 +0100

    Updated main.h

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 #ifndef MAIN_H
 #define MAIN_H
@@ -12,8 +13,15 @@
  */
 typedef struct formats
 {
-    char *format;
-    char (*func)(char text);
+	char *format;
+	void (*func)(char *);
 } ftype;
+
+void (*get_format(char *s))(char *);
+
+int count(char *formats);
+void _printf(char *formats, ...);
+
+void format_string(char *formats);
 
 #endif
