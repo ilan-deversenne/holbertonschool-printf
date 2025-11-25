598730f1e3d3b58ca575fe02b0e635de62a00ba2
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-25 11:12:28 +0100

    feat: Added format functions

---
+++ format_functions.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/*
+  * format_char
+  */
+void format_char(char *arg)
+{
+	printf("format_char->formats: %s\n", arg);
+}
+
+/*
+  * format_string
+  */
+void format_string(char *arg)
+{
+	printf("format_string->formats: %s\n", arg);
+}
