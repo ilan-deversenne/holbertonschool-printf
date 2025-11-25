3f16315e13cb95d9138b52fda81a93cd7e043ad4
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-25 11:12:55 +0100

    feat: Added main.c for test

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+
+		Fix:
+			Le call prend tous se qu'il y'a deriere le % qui lui est affecter
+
+		Fix:
+			Call always return NULL
+
+*/
+
+int main()
+{
+	_printf("Hey, %s", "Jean");
+
+	return (0);
+}
