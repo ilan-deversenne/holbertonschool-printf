4c58f97677ca183d13389d597c95cefd27293a96
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-11-27 10:24:13 +0100

    fix: Remove debug & count function

--- _printf.c
+++ _printf.c
@@ -1,33 +1,10 @@
 #include "main.h"
 
-int count(char *formats)
-{
-	int index, length;
-
-	index = 0;
-	length = 0;
-	while (*(formats + index) != '\0')
-	{
-		if (*(formats + index) == '%')
-		{
-			/*call = get_format(formats + i);
-			if (call != NULL) */
-
-			index += 2;
-			continue;
-		}
-
-		index++;
-		length++;
-	}
-
-	return (length);
-}
-
 void _printf(char *formats, ...)
 {
-	va_list args;
+	void *arg;
 	int index;
+	va_list args;
 	char character;
 	void (*call)(void *);
 
@@ -41,8 +18,6 @@ void _printf(char *formats, ...)
 			call = get_format(formats + index + 1);
 			if (call != NULL)
 				call(va_arg(args, void *));
-			else
-				printf("call: NULL\n"); /* DEBUG */
 
 			index += 2;
 			continue;
